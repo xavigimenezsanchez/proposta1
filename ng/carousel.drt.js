504a93f779a7677d5297a52d3ad5537aecb6a73d
xavigimenezsanchez <javiergimenezsanchez@gmail.com>
2015-09-14 15:48:47 +0000

    Added directive carousel and efects jquery.transit.min.js

---
+++ ng/carousel.drt.js
@@ -0,0 +1,31 @@
+angular.module('appEnplater')
+    .directive('carousel', function() {
+       return {
+           restrict:    'E',
+           templateUrl: 'carousel-template.html',
+           link: function(scope,element,attr) {
+               console.log('Carousel loaded');
+                $('.carousel').carousel({
+                      interval: 300000
+                    });
+                 $('.carousel .active .row p.u').transition({ y: -210 },500,'snap');; 
+                $('.carousel').on('slid.bs.carousel', function () {
+                    
+                    $('.carousel .active .row p.u').transition({ y: -210 },500,'snap');
+                    $('.carousel .active .row p.q').transition({
+                              perspective: '100px',
+                              rotateX:  '360deg'
+                            },500,'snap');
+                });
+                
+                $('.carousel').on('slide.bs.carousel', function () {
+                    $('.carousel .active .row p.u').transition({ y: 210 },500,'snap');
+                    $('.carousel .active .row p.q').transition({
+                              perspective: '100px',
+                              rotateX:  '-360deg'
+                            },500,'snap');
+                });
+                    
+           }
+       };
+    });
